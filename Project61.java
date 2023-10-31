dc1f352d5b9df4d8a16a6d84875af049b3367df4
hemasahithi <148951287+hemasahithi@users.noreply.github.com>
2023-10-31 14:52:41 +0530

    Add files via upload

---
+++ Project61.java
@@ -0,0 +1,13 @@
+package assistedproject;
+
+public class Project61 {
+	public Project61(String name) {
+		System.out.println("Passed Name is :"+ name);
+	}
+	public static void main(String[] args) {
+		Project61 myProject61 = new Project61("tommy");
+		
+	}
+
+}
+
