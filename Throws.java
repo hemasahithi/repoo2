cf7905fe701675dbb260800835fbecb9e84d58cd
hemasahithi <148951287+hemasahithi@users.noreply.github.com>
2023-10-31 15:33:56 +0530

    Add files via upload

---
+++ Throws.java
@@ -0,0 +1,18 @@
+package assistedpro31;
+
+public class Throws {
+
+	public static void validate(int age) {
+		if(age<18)  
+			throw new ArithmeticException("not valid");  
+			else
+			System.out.println("welcome to vote");  
+			   }  
+			public static void main(String args[]){  
+			validate(13);  
+			System.out.println("rest of the code...");  
+
+
+	}
+
+}
